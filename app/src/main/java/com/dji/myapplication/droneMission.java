621a3f7ba21edb0424a707a8f6909cb7ce1adf89
Andrew Thompson <athompson2205@aol.com>
2021-07-11 13:29:31 -0700

    init commit

---
+++ app/src/main/java/com/dji/myapplication/droneMission.java
@@ -0,0 +1,39 @@
+package com.dji.myapplication;
+import dji.sdk.mission.MissionControl;
+//import dji.sdk.mission.timeline.*;
+import dji.sdk.mission.timeline.actions.*;
+//import dji.sdk.mission.timeline.actions.MissionAction;
+public class droneMission
+{
+    private MissionControl missiona,missionEnd;;
+    private float height = (float) 15.00;
+    //private float angle = (float)50.00;
+    //setup for the missions, adding actions to be executed sequentially
+    //triggers can also be added to intersperse actions using other code, will explore that aspect
+    //more after verifiying the usage of the mission timeline
+    public droneMission()
+    {
+        missiona.scheduleElement(new TakeOffAction());
+        //note: I believe that if called succsecivly, gotoaction will make the drone
+        //raise itself twice as high, or atleast 15 meters from current position.
+        //double tap with caution.
+        missiona.scheduleElement(new GoToAction(height));
+        missiona.scheduleElement(new AircraftYawAction(-100,false));
+        missiona.scheduleElement(new AircraftYawAction(100,false));
+        missiona.scheduleElement(new AircraftYawAction(-100,false));
+        missiona.scheduleElement(new AircraftYawAction(100,false));
+        missionEnd.scheduleElement(new GoHomeAction());
+    }
+    //starts the mission, or stops it
+    //expected behavior may not be accurate, testing needed if this works
+    protected void runMission()
+    {
+        if(missiona.isTimelineRunning())
+        {
+            missiona.stopTimeline();
+            missionEnd.startTimeline();
+        }
+        else
+            missiona.startTimeline();
+    }
+}
