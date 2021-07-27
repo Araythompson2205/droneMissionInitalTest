c81de0ecc81efb1d878c5d720bbf8b86dbd648b3
Andrew Thompson <athompson2205@aol.com>
2021-07-27 12:26:22 -0700

    fixed crash

--- app/src/main/java/com/dji/myapplication/droneMission.java
+++ app/src/main/java/com/dji/myapplication/droneMission.java
@@ -1,10 +1,14 @@
 package com.dji.myapplication;
+import androidx.annotation.Nullable;
+
+import dji.common.error.DJIError;
 import dji.sdk.mission.MissionControl;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 //import dji.sdk.mission.timeline.*;
 import dji.sdk.mission.timeline.TimelineElement;
+import dji.sdk.mission.timeline.TimelineEvent;
 import dji.sdk.mission.timeline.actions.*;
 //import dji.sdk.mission.timeline.actions.MissionAction;
 public class droneMission
@@ -20,7 +24,9 @@ public class droneMission
     {
         elements= new ArrayList<>();
         missiona=MissionControl.getInstance();
+        MissionControl.Listener listener = (timelineElement, timelineEvent, djiError) -> {
 
+        };
         elements.add(new TakeOffAction());
         //note: I believe that if called succsecivly, gotoaction will make the drone
         //raise itself twice as high, or atleast 15 meters from current position.
@@ -32,6 +38,7 @@ public class droneMission
         elements.add(new AircraftYawAction(100,false));
        // missionEnd.scheduleElement(new GoHomeAction());
         missiona.scheduleElements(elements);
+        missiona.addListener(listener);
     }
     //starts the mission, or stops it
     //expected behavior may not be accurate, testing needed if this works
